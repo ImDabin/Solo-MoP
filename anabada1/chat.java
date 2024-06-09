dd2a343745f51feaf9ce165525e695cb55d71816
Dabin <dabin0319@gmail.com>
2024-06-09 22:02:57 +0900

    ver1.5
    홈 빼고 전부 구현

--- anabada1/chat.java
+++ anabada1/chat.java
@@ -3,6 +3,8 @@ package com.example.anabada1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -45,6 +47,16 @@ public class chat extends AppCompatActivity {
                     default:
                         return false;
                 }
+            }
+        });
+
+        // inchat1 버튼 클릭 시 InChatActivity로 이동하도록 설정
+        Button inchatButton1 = findViewById(R.id.inchat123);
+        inchatButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(chat.this, inchat.class);
+                startActivity(intent);
             }
         });
     }
